4a38bff32d785398405ac50ea8281aba86c9fcad
Anviksha Jain <56529970+Anvikshajain@users.noreply.github.com>
2020-08-14 09:25:28 +0530

    Update STRUCTURE.cpp

--- STRUCTURE.cpp
+++ STRUCTURE.cpp
@@ -1,1 +1,25 @@
+/*
+@ file name
+@ code by author_name
+@ two line description of the code
+*/
 
+#include<iostream.h>
+
+//doing sum of two integers
+void do_sum(int a,int b)
+{
+  int c=a+b;
+  cout<<"Sum of two numbers is : "<<c<<endl;
+}
+
+int main()
+{
+  //passing values to function for 
+  //adding them
+  do_sum(5,8);
+  return 0;
+}
+
+//comment down every required line for better 
+// understanding of code
